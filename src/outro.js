1534ca34bde8dd7db2fcedc223e6ed5d8d4c48b9
Steven Parkes <smparkes@smparkes.net>
2009-11-03 15:10:04 -0800

    Envjs symbol has to be put in the window before the window content is loaded

--- src/outro.js
+++ src/outro.js
@@ -3,6 +3,27 @@
 //   (function(){
 //     function(){
 
+      // User accesible interface ...
+      var Envjs = $w.Envjs = $env.Envjs = function(){
+        if(arguments.length === 2){
+          for ( var i in arguments[1] ) {
+    	    var g = arguments[1].__lookupGetter__(i), 
+            s = arguments[1].__lookupSetter__(i);
+    	    if ( g || s ) {
+    	      if ( g ) $env.__defineGetter__(i, g);
+    	      if ( s ) $env.__defineSetter__(i, s);
+    	    } else
+    	      $env[i] = arguments[1][i];
+          }
+        }
+        if (arguments[0] != null && arguments[0] != "")
+          window.location = arguments[0];
+      };
+      Envjs.$env = $env;
+      Envjs.wait = $env.wait;
+      Envjs.interpreter = window.whichInterpreter;
+      Envjs.evaluate = $env.$master.evaluate;
+  
       $w.__loadAWindowsDocument__(options.url || "about:blank");
     };
 
@@ -13,27 +34,6 @@
   // Initial window setup
   $env.init.call(this);
 
-  // User accesible interface ...
-  Envjs = $env.Envjs = function(){
-    if(arguments.length === 2){
-      for ( var i in arguments[1] ) {
-    	var g = arguments[1].__lookupGetter__(i), 
-            s = arguments[1].__lookupSetter__(i);
-    	if ( g || s ) {
-    	  if ( g ) $env.__defineGetter__(i, g);
-    	  if ( s ) $env.__defineSetter__(i, s);
-    	} else
-    	  $env[i] = arguments[1][i];
-      }
-    }
-    if (arguments[0] != null && arguments[0] != "")
-      window.location = arguments[0];
-  };
-  Envjs.$env = $env;
-  Envjs.wait = $env.wait;
-  Envjs.interpreter = window.whichInterpreter;
-  Envjs.evaluate = $env.$master.evaluate;
-  
 })();
 
 // Local Variables:
